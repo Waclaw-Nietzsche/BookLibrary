4e13b63cc2f558a10cadc6f91b1ae4026b9addc9
waclaw nietzsche <vaclavstrelnikov@gmail.com>
2020-04-04 17:41:02 +0300

    Initial commit

---
+++ Book.Core/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Book.Core.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            Books = new Collection<Book>();
+        }
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ Book.Core/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Book.Core.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
